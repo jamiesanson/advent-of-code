68bb4a32b51af583f15078c7a9f5b55b0e818e9f
Jamie Sanson <jamie@sanson.dev>
2021-12-02 20:49:37 +1300

    Day 1 part 2

--- src/main/kotlin/day1/Day1.kt
+++ src/main/kotlin/day1/Day1.kt
@@ -1,5 +1,20 @@
 package day1
 
-fun Day1(): String {
-    return ""
+fun Day1(input: String): String {
+    return input
+        .split('\n')
+        .windowed(2)
+        .map { (prev, current) -> if (current.toInt() > prev.toInt()) 1 else 0 }
+        .sum().toString()
 }
+
+fun Day1Part2(input: String): String {
+    return input
+        .split('\n')
+        .windowed(3)
+        .map { (one, two, three) -> one.toInt() + two.toInt() + three.toInt() }
+        .windowed(2)
+        .map { (prev, current) -> if (current > prev) 1 else 0 }
+        .sum().toString()
+}
+K
