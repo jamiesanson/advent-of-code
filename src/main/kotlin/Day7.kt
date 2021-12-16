7bee68fc83e215d0bb9effa945858c664214af61
Jamie Sanson <jamie@sanson.dev>
2021-12-16 21:35:31 +1300

    Day 7 - faketorial is definitely not the right term here

---
+++ src/main/kotlin/Day7.kt
@@ -0,0 +1,36 @@
+import java.lang.Math.abs
+
+fun Day7Part1(input: List<String>): Int {
+    val startingPos = input.first().split(",").map { it.toInt() }
+
+    val min = startingPos.minOrNull()!!
+    val max = startingPos.maxOrNull()!!
+
+    var minFuel = Int.MAX_VALUE
+
+    for (a in min..max) {
+        val fuel = startingPos.map { abs(it - a) }.sum()
+        if (fuel < minFuel) minFuel = fuel
+    }
+
+    return minFuel
+}
+
+
+fun Day7Part2(input: List<String>): Int {
+    val startingPos = input.first().split(",").map { it.toInt() }
+
+    fun Int.faketorial() = if (this <= 1) this else (1..this).reduce { acc, i -> acc + i }
+
+    val min = startingPos.minOrNull()!!
+    val max = startingPos.maxOrNull()!!
+
+    var minFuel = Int.MAX_VALUE
+
+    for (a in min..max) {
+        val fuel = startingPos.map { abs(it - a).faketorial() }.sum()
+        if (fuel < minFuel) minFuel = fuel
+    }
+
+    return minFuel
+}
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,5 @@ import java.io.File
 fun main(args: Array<String>) {
     val input = File("input").readLines()
 
-    println("Day 6 pt 2: ${Day6Part2(input)}")
+    println("Result: ${Day7Part2(input)}")
 }
