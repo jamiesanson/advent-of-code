ee6f3d8057da4c02913932300a020de23d590762
Jamie Sanson <jamie@sanson.nz>
2024-11-27 19:09:29 +0000

    Prepare for day 1

---
+++ src/main/kotlin/mmxxiiii/Day01.kt
@@ -0,0 +1,15 @@
+package mmxxiiii
+
+import mmxxiiii.input.input
+
+fun Day01Part1(lines: List<String>): Int {
+    return lines.sumOf { line -> line.toInt() }
+}
+
+fun Day01Part2(lines: List<String>): Int {
+    return lines.sumOf { line -> line.toInt() }
+}
+
+fun main() {
+    val input = input(day = 1)
+}
---
+++ src/main/kotlin/mmxxiiii/input/input.kt
@@ -0,0 +1,5 @@
+package mmxxiiii.input
+
+import util.Input
+
+fun input(day: Int): Input = Input(year = "mmxxiiii", day = day)
